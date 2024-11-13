e6efd8ccec49afd1377acc0b01d5325fa2050962
bern023 <150527708+bern023@users.noreply.github.com>
2024-11-14 12:38:28 +1300

    Update scripts.js

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -24,3 +24,22 @@ function grabWeather(event) {
     }) 
     .catch(error => console.error('Error:', error)); 
 }
+//Bw Today page
+function openInfo(evt, tabName) {
+  //declare the variables
+  var i, tabcontent, tablinks;
+  // get the elements and hide them
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for(i=0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+  // remove the 'active' class from elements
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active","")
+  }
+  //add active class to the clicked button 
+  document.getElementById(tabName).style.display = "block";
+  evt.currentTarget.className += " active";
+
+}
